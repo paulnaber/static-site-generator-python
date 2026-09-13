2f7e61e237a8927e653d9a468a76921eaafb93fa
Paul Naber <paul.naber@gmx.de>
2026-09-13 20:03:30 +0200

    add regex for links and images

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,14 @@
+import re
+
 from textnode import TextNode, TextType
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 
 def split_nodes_delimiter(
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+    extract_markdown_images,
+    extract_markdown_links,
+    split_nodes_delimiter,
+)
 from textnode import TextNode, TextType
 
 
@@ -78,6 +82,56 @@ class TestSplitNodesDelimiter(unittest.TestCase):
             [TextNode("**bold**", TextType.TEXT)], "**", TextType.BOLD
         )
         self.assertEqual(nodes, [TextNode("bold", TextType.BOLD)])
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")], matches
+        )
+
+    def test_extract_multiple_markdown_images(self):
+        matches = extract_markdown_images(
+            "![one](https://example.com/one.png) and "
+            "![two](https://example.com/two.jpg)"
+        )
+        self.assertListEqual(
+            [
+                ("one", "https://example.com/one.png"),
+                ("two", "https://example.com/two.jpg"),
+            ],
+            matches,
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) "
+            "and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
+
+    def test_links_do_not_include_images(self):
+        matches = extract_markdown_links(
+            "![image](https://example.com/image.png) [link](https://example.com)"
+        )
+        self.assertListEqual([("link", "https://example.com")], matches)
+
+    def test_extract_empty_alt_and_anchor_text(self):
+        self.assertListEqual(
+            [("", "https://example.com/image.png")],
+            extract_markdown_images("![](https://example.com/image.png)"),
+        )
+        self.assertListEqual(
+            [("", "https://example.com")],
+            extract_markdown_links("[](https://example.com)"),
+        )
 
 
 if __name__ == "__main__":
