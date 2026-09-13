c818624fb9e039f78483d8301f22a52606290958
Paul Naber <paul.naber@gmx.de>
2026-09-13 21:10:56 +0200

    generate all content pages recursively

--- src/main.py
+++ src/main.py
@@ -38,9 +38,23 @@ def generate_page(from_path, template_path, dest_path):
         destination_file.write(page)
 
 
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for entry in os.listdir(dir_path_content):
+        content_path = os.path.join(dir_path_content, entry)
+        destination_path = os.path.join(dest_dir_path, entry)
+
+        if os.path.isfile(content_path) and entry.endswith(".md"):
+            destination_path = destination_path[:-3] + ".html"
+            generate_page(content_path, template_path, destination_path)
+        elif os.path.isdir(content_path):
+            generate_pages_recursive(
+                content_path, template_path, destination_path
+            )
+
+
 def main():
     copy_static_to_public("static", "public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "public")
 
 
 if __name__ == "__main__":
